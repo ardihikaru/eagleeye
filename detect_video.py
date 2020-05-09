8e24420c9a7c82036353859e1be6032e82df53e5
Ardihikaru <ardihikaru3@gmail.com>
2020-05-09 15:49:19 +0800

    added testing code for reading video stream

--- detect_video.py
+++ detect_video.py
@@ -1,30 +1,41 @@
 import cv2 as cv
+from libs.addons.streamer.my_fps import MyFPS
 
 
 if __name__ == "__main__":
     # Ardi: Use video instead
     print("\nReading video:")
-    # cap = cv.VideoCapture("data/5g-dive/customTest_MIRC-Roadside-5s.mp4")
-    cap = cv.VideoCapture("http://www.opticodec.com/test/tropic.mp4")
-    # cap = cv.VideoCapture("http://140.113.86.92:10000/drone-3.flv")
+
     cv.namedWindow("Image", cv.WND_PROP_FULLSCREEN)
-    cv.resizeWindow("Image", 1366, 768) # Enter your size
-    print("ok sampe sini")
-    while (cap.isOpened()):
-        print("masuk while")
+    cv.moveWindow("Image", 0, 0)
+    cv.resizeWindow("Image", 1366, 768)  # Enter your size
+
+    # cap = cv.VideoCapture("data/5g-dive/videos/customTest_MIRC-Roadside-5s.mp4")
+    cap = cv.VideoCapture("data/5g-dive/videos/customTest_MIRC-Roadside-20s.mp4")
+    # cap = cv.VideoCapture("http://140.113.86.92:10000/drone-3.flv")
+    capture_lat = []
+
+    fps = MyFPS().start()
+    while cap.isOpened():
         # ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
         try:
             ret, frame = cap.read()
 
             cv.imshow("Image", frame)
+            print("[CURRENT INFO] approx. FPS: {:.2f}".format(fps.current_fps()))
         except:
             print("No more frame to show.")
             break
 
-        if cv.waitKey(10) & 0xFF == ord('q'):
+        if cv.waitKey(1) & 0xFF == ord('q'):
             break
+        fps.update()
 
-    print("selesai")
+    # stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
     # The following frees up resources and closes all windows
     cap.release()
     cv.destroyAllWindows()
