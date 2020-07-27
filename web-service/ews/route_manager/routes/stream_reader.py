6713ca4b8e59e88a7d0b39c392285b5142bcfeb0
Ardihikaru <ardihikaru3@gmail.com>
2020-07-28 06:42:48 +0800

    renamed API /api/stream into /api/stream/live; added /api/stream/folder

--- web-service/ews/route_manager/routes/stream_reader.py
+++ web-service/ews/route_manager/routes/stream_reader.py
@@ -10,11 +10,43 @@ from ext_lib.utils import get_unprocessable_request
 route = RouteCollector()
 
 
-@route('', methods=['POST', 'OPTIONS'])
-async def stream_raw(request):
+@route('/live', methods=['POST', 'OPTIONS'])
+async def stream_live(request):
     """
         Endpoint to start streaming
-        Try: curl http://localhost:8080/api/auth/login -X POST -H "Content-Type: application/json" -d '{"username":"ardi", "password": "ardi"}'
+        Try: curl http://localhost:8080/api/stream/live -X POST -H "Content-Type: application/json"
+                -d '{
+                        "raw": false,
+                        "algorithm": "YOLOv3",
+                        "uri": "rtmp://140.113.86.98:15500/live/demo",
+                        "exec": true,
+                        "worker": 1
+                    }'
+    """
+    # uri = request.match_info.get('uri', "Anonymous")
+
+    try:
+        if request.method == 'POST':
+            request_json = await request.json()
+            resp = DataController().read(request_json)
+            return aiohttp.web.json_response(resp)
+    except Exception as e:
+        return get_unprocessable_request()
+        # return aiohttp.web.HTTPBadRequest()
+
+
+@route('/folder', methods=['POST', 'OPTIONS'])
+async def stream_folder(request):
+    """
+        Endpoint to start streaming
+        Try: curl http://localhost:8080/api/stream/live -X POST -H "Content-Type: application/json"
+                -d '{
+                        "raw": false,
+                        "algorithm": "YOLOv3",
+                        "uri": "common_files/5g-dive/57-frames",
+                        "exec": true,
+                        "worker": 1
+                    }'
     """
     # uri = request.match_info.get('uri', "Anonymous")
 
