b183d1d782fd7b0747a511be01b842b7658650f3
ardihikaru <ardihikaru3@gmail.com>
2020-08-05 15:50:07 +0800

    Added frame_id to tag each latency data

--- web-service/ews/database/latency/latency.py
+++ web-service/ews/database/latency/latency.py
@@ -2,12 +2,14 @@
     Class Model for `Latency` Collections
 """
 
-from mongoengine import Document, StringField, DateTimeField, FloatField
+from mongoengine import Document, StringField, IntField, FloatField
 import datetime
 
 
+# TODO: Add `drone_id` field to tag the image source
 class LatencyModel(Document):
     meta = {'collection': 'Latency'}
+    frame_id = IntField(required=True)
     category = StringField(required=True)
     algorithm = StringField(required=True)
     section = StringField(required=True)
