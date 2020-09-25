e2d5cb329f8b3ff14bdfc71447d8727c1af69432
timwilliam <timothywilliam.cs06g@g2.nctu.edu.tw>
2020-09-25 14:50:32 +0800

    Reorganized the k8s config files for easier deployment

--- orchestration-gpu/deployment/kustomize-renew.sh
+++ orchestration-gpu/deployment/kustomize-renew.sh
@@ -3,4 +3,6 @@ kubectl kustomize ./redis > redis.yaml
 kubectl kustomize ./ews > ews.yaml
 kubectl kustomize ./detection > detection.yaml
 kubectl kustomize ./scheduler > scheduler.yaml
-kubectl kustomize . > start-ee.yaml
+kubectl kustomize ./volume > volume.yaml
+kubectl kustomize ./volume-claim > volume-claim.yaml
+kubectl kustomize ./service > service.yaml
