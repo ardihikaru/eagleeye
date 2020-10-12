462109ad522fdd2021a806fc4e7c974904596ae9
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2020-10-12 15:12:12 +0800

    add Log information when initializing the modules

--- web-service/ews/route_manager/module.py
+++ web-service/ews/route_manager/module.py
@@ -2,6 +2,15 @@ import asab
 from .service import RouteManagerService
 from .handler import RouteWebHandler
 from .rthandler import WebRealtimeHandler
+from ext_lib.utils import get_current_time
+import logging
+
+###
+
+L = logging.getLogger(__name__)
+
+
+###
 
 
 class RouteManagerModule(asab.Module):
@@ -11,3 +20,7 @@ class RouteManagerModule(asab.Module):
 		self.Service = RouteManagerService(app)
 		self.WebHandler = RouteWebHandler(app)
 		self.WebRTHandler = WebRealtimeHandler(app)
+
+	async def initialize(self, app):
+		L.warning("\n[%s] Initialize RouteManagerModule." % get_current_time())
+
