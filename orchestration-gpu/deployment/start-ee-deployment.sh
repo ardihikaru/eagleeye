0680fb71ac5d6fc0d7f5aacd48c4a92683646d0e
Tim William <timothywilliam.cs06g@g2.nctu.edu.tw>
2020-10-14 17:34:14 +0800

    Minor modification to add 2 worker node, working now

--- orchestration-gpu/deployment/start-ee-deployment.sh
+++ orchestration-gpu/deployment/start-ee-deployment.sh
@@ -1,5 +1,7 @@
 kubectl apply -f redis.yaml
 kubectl apply -f mongo.yaml
 kubectl apply -f ews.yaml
+sleep 5
 kubectl apply -f scheduler.yaml
+sleep 5
 kubectl apply -f detection.yaml
--- orchestration-gpu/scripts/stream-rtmp.sh
+++ orchestration-gpu/scripts/stream-rtmp.sh
@@ -2,6 +2,6 @@ curl --location --request POST 'http://127.0.0.1:31000/api/stream/live' \
 --data-raw '{
     "algorithm": "YOLOv3",
     "stream": true,
-    "uri": "rtmp://140.113.86.92/live/demo",
+    "uri": "rtsp://140.113.86.98:40000/test",
     "scalable": true
 }'
