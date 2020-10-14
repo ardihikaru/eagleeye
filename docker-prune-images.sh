f31c677faaebc16bd2e1ef4033c9473c8507c639
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2020-10-15 02:54:28 +0800

    add shell scripts to deploy in the docker environment

---
+++ docker-prune-images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Capturing input arguments
+VERSION=$1
+
+# Verify inputs
+if [ -z "$VERSION" ]
+then
+      echo "\$VERSION is empty"
+      VERSION="1.0"  # default value
+else
+      echo "\$VERSION is NOT empty"
+fi
+
+docker image rm -f "5g-dive/eagleeye/visualizer-service:${VERSION}"
+docker image rm -f "5g-dive/eagleeye/dual-object-detection-service:${VERSION}"
+docker image rm -f "5g-dive/eagleeye/scheduler-service:${VERSION}"
+docker image rm -f "5g-dive/eagleeye/dual-object-detection-service-head:${VERSION}"
+docker image rm -f "5g-dive/eagleeye/web-service:${VERSION}"
+docker image rm -f "5g-dive/eagleeye/nvidia-gpu-opencv:${VERSION}"
