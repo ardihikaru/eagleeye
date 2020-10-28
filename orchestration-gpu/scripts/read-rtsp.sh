944329ccaef623e895fc4cafa7a53dda12499ab0
Tim William <timothywilliam.cs06g@g2.nctu.edu.tw>
2020-10-28 14:26:50 +0800

    Hotfix for EE

--- orchestration-gpu/scripts/read-rtsp.sh
+++ orchestration-gpu/scripts/read-rtsp.sh
@@ -2,6 +2,6 @@ curl --location --request POST 'http://127.0.0.1:31000/api/stream/live' \
 --data-raw '{
     "algorithm": "YOLOv3",
     "stream": true,
-    "uri": "rtsp://140.113.86.92/input",
+    "uri": "rtsp://192.168.1.250/0137",
     "scalable": true
 }'
