db9da0ed364fd84f04b79117c23e949109835166
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2020-11-04 07:02:41 +0800

    update printing info

--- register-node.sh
+++ register-node.sh
@@ -1,7 +1,10 @@
+printf "\n>>> Trying to register a new node . . . \n\n"
+
 curl --location --request POST 'http://localhost:8080/api/nodes' \
     --header 'Content-Type: application/json' \
     --data '{
         "candidate_selection": true,
         "persistence_validation": true
     }'
-echo "Regestering a new node succeed"
+
+printf "\n\n>>> Regestering a new node succeed\n\n"
