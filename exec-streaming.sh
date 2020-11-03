f33f3c4d99ece81347ddbd266002d20e20e59e3b
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2020-11-04 07:45:40 +0800

    add bash script to start streaming

---
+++ exec-streaming.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Capturing input arguments
+HOST=$1
+PORT=$2
+FPATH=$3
+MODE=$4
+
+printf "\n>>> FYI: Arg[1]=%s; Arg[2]=%s; Arg[3]=%s; Arg[4]=%s \n\n" \
+  "HOST (default=localhost)" \
+  "PORT (default=8080)" \
+  "FPATH (default=/home/s010132/devel/eagleeye/data/5g-dive/videos/customTest_MIRC-Roadside-5s.mp4)" \
+  "MODE (default=STREAM)"
+
+if [ -z "$HOST" ]
+then
+      echo "\$HOST is empty"
+      HOST="localhost"  # default value
+fi
+
+if [ -z "$PORT" ]
+then
+      echo "\$PORT is empty"
+      PORT="8080"  # default value
+fi
+
+if [ -z "$FPATH" ]
+then
+      echo "\$FPATH is empty"
+      FPATH="/home/s010132/devel/eagleeye/data/5g-dive/videos/customTest_MIRC-Roadside-5s.mp4"  # default value
+fi
+
+if [ -z "$MODE" ]
+then
+      echo "\$MODE is empty"
+      MODE="STREAM"  # default value
+fi
+
+printf "\n>>> Web Service URL is [http://%s:%s]" ${HOST} ${PORT}
+printf "\n>>> API to start capturing streaming URL is [http://%s:%s/api/stream/live]" ${HOST} ${PORT}
+printf "\n>>> VIDEO FILE PATH is [%s]" ${FPATH}
+printf "\n>>> Capturing Mode is [%s]\n\n" ${MODE}
+
+
+curl --location --request POST "http://${HOST}:${PORT}/api/stream/live" \
+    --header "Content-Type: application/json" \
+    --data "{
+        \"algorithm\": \"YOLOv3\",
+        \"stream\": \"${MODE}\",
+        \"uri\": \"${FPATH}\",
+        \"scalable\": true
+    }"
+
+printf "\n\n>>> EagleEYE started to capture and detect PiH objects\n\n"
