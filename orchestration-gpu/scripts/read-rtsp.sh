ce0da1b0d81c19bd2722893583530f5adffcbc7b
Tim William <timothywilliam.cs06g@g2.nctu.edu.tw>
2020-11-06 10:18:15 +0800

    Update to the streaming script to conform with the latest update in 2.3

--- orchestration-gpu/scripts/read-rtsp.sh
+++ orchestration-gpu/scripts/read-rtsp.sh
@@ -1,7 +1,7 @@
 curl --location --request POST 'http://140.113.86.92:31000/api/stream/live' \
 --data-raw '{
     "algorithm": "YOLOv3",
-    "stream": true,
-    "uri": "rtsp://10.194.188.106/1028",
+    "stream": "STREAM",
+    "uri": "rtsp://140.113.86.92/test",
     "scalable": true
 }'
