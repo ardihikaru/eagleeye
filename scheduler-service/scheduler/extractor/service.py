423ce2b5ab64602b46c992556f56c9239542046e
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2020-11-11 19:06:34 +0800

    bugfix: receiving images through TCP protocol

--- scheduler-service/scheduler/extractor/service.py
+++ scheduler-service/scheduler/extractor/service.py
@@ -40,6 +40,8 @@ class ExtractorService(asab.Service):
 		self.half = bool(asab.Config["objdet:yolo"]["half"])
 		self.source_folder_prefix = asab.Config["objdet:yolo"]["source_folder_prefix"]
 		self.file_ext = asab.Config["objdet:yolo"]["file_ext"]
+		self._is_auto_restart = bool(int(asab.Config["stream:config"]["auto_restart"]))
+		self._restart_delay = float(asab.Config["stream:config"]["restart_delay"])
 
 		self.executor = ThreadPoolExecutor(int(asab.Config["thread"]["num_executor"]))
 
@@ -237,8 +239,8 @@ class ExtractorService(asab.Service):
 
 		try:
 			while True:
-				success, frame_id, t0_zmq_soure, frame = get_imagezmq(zmq_source_reader)
-				t1_zmq_soure = (time.time() - t0_zmq_soure) * 1000
+				success, frame_id, t0_zmq_source, frame = get_imagezmq(zmq_source_reader)
+				t1_zmq_soure = (time.time() - t0_zmq_source) * 1000
 				self.frame_id = int(frame_id)
 
 				# Save latency of receiving image from souce
@@ -323,107 +325,111 @@ class ExtractorService(asab.Service):
 
 	async def extract_image_tcp(self, config):
 		L.warning("#### I am extractor TCP function from ExtractorService!")
+
+		# Set ZMQ Sender
 		senders = self.ZMQService.get_senders()
 
 		# extract `uri` to collect `host` and `port`
 		uri_detail = config["uri"].split(":")
 
-		# Set ZMQ Source Reader
+		while self._is_auto_restart:
+			try:
+				# Reset frame_id
+				self.frame_id = 0
+				await self._stream_tcp_data(config, uri_detail, senders)
+
+			except Exception as e:
+				L.error("[ERROR] extractor/service.py > def extract_image_tcp(): %s" % str(e))
+				L.warning("... Restarting connection in {} seconds ...".format(self._restart_delay))
+				time.sleep(self._restart_delay)
+				# return False
+
+	async def _stream_tcp_data(self, config, uri_detail, senders):
+		# Set TCP Connection
 		await self.TCPCameraServerService.initialize_camera_source_reader(
 			_tcp_host=uri_detail[0],
 			_tcp_port=int(uri_detail[1])
 		)
-		tcp_conn = self.TCPCameraServerService.get_tcp_conn()
 
-		try:
-			# Reset frame_id
-			self.frame_id = 0
+		while True:
+			self.frame_id += 1
 
-			while True:
-				self.frame_id += 1
+			success, frame_id, t0_zmq_source, frame = await self.TCPCameraServerService.get_image(self.frame_id)
+			# t1_zmq_soure = (time.time() - t0_zmq_source) * 1000
 
-				success, frame_id, t0_zmq_soure, frame = await self.TCPCameraServerService.get_image(self.frame_id)
-				# t1_zmq_soure = (time.time() - t0_zmq_soure) * 1000
+			# # Save latency of receiving image from souce
+			# await self._save_latency(
+			# 	self.frame_id, t1_zmq_soure, "-", "imagezmq", "reading_source"
+			# )
+			# L.warning('\n[%s] Communication Latency of %s for frame-%s (%.3f ms)' % (
+			# 	get_current_time(), "[Receiving Image with IMAGEZMQ]", str(self.frame_id), t1_zmq_soure))
 
-				# # Save latency of receiving image from souce
-				# await self._save_latency(
-				# 	self.frame_id, t1_zmq_soure, "-", "imagezmq", "reading_source"
-				# )
-				# L.warning('\n[%s] Communication Latency of %s for frame-%s (%.3f ms)' % (
-				# 	get_current_time(), "[Receiving Image with IMAGEZMQ]", str(self.frame_id), t1_zmq_soure))
+			# Sending image data into Visualizer Service as well
+			self.ZMQService.send_image_to_visualizer(self.frame_id, frame)
 
-				# Sending image data into Visualizer Service as well
-				self.ZMQService.send_image_to_visualizer(self.frame_id, frame)
+			# Start t0_e2e_lat: To calculate the e2e processing & comm. latency
+			t0_e2e_lat = time.time()
 
-				# Start t0_e2e_lat: To calculate the e2e processing & comm. latency
-				t0_e2e_lat = time.time()
+			# Perform scheduling based on Round-Robin fasion (Default)
+			t0_sched_lat = time.time()
+			try:
+				if bool(int(asab.Config["stream:config"]["test_mode"])):
+					sel_node_id = 0
+				else:
+					sel_node_id = await self.SchPolicyService.schedule(max_node=len(senders["node"]))
+				L.warning("Selected Node idx: %s" % str(sel_node_id))
+			except Exception as e:
+				L.error("[ERROR]: %s" % str(e))
+			t1_sched_lat = (time.time() - t0_sched_lat) * 1000
+			# TODO: To implement scheduler here and find which node will be selected
 
-				# Perform scheduling based on Round-Robin fasion (Default)
-				t0_sched_lat = time.time()
-				try:
-					if bool(int(asab.Config["stream:config"]["test_mode"])):
-						sel_node_id = 0
-					else:
-						sel_node_id = await self.SchPolicyService.schedule(max_node=len(senders["node"]))
-					L.warning("Selected Node idx: %s" % str(sel_node_id))
-				except Exception as e:
-					L.error("[ERROR]: %s" % str(e))
-				t1_sched_lat = (time.time() - t0_sched_lat) * 1000
-				# TODO: To implement scheduler here and find which node will be selected
+			# First, notify the Object Detection Service to get ready (publish)
+			node_id = senders["node"][sel_node_id]["id"]
+			node_channel = senders["node"][sel_node_id]["channel"]
+			node_name = senders["node"][sel_node_id]["name"]
 
-				# First, notify the Object Detection Service to get ready (publish)
-				node_id = senders["node"][sel_node_id]["id"]
-				node_channel = senders["node"][sel_node_id]["channel"]
-				node_name = senders["node"][sel_node_id]["name"]
+			L.warning("NodeID=%s; NodeChannel=%s; NodeName=%s" % (str(node_id), node_channel, node_name))
 
-				L.warning("NodeID=%s; NodeChannel=%s; NodeName=%s" % (str(node_id), node_channel, node_name))
+			# Save Scheduling latency
+			await self._save_latency(
+				self.frame_id, t1_sched_lat, "Round-Robin", "scheduling", "Scheduling", node_id, node_name
+			)
+			L.warning('\n[%s] Proc. Latency of %s for frame-%s (%.3f ms)' % (
+				get_current_time(), "scheduling", str(self.frame_id), t1_sched_lat))
 
-				# Save Scheduling latency
-				await self._save_latency(
-					self.frame_id, t1_sched_lat, "Round-Robin", "scheduling", "Scheduling", node_id, node_name
+			# Save e2e latency
+			self._exec_e2e_latency_collector(t0_e2e_lat, node_id, self.frame_id)
+
+			# send data into Scheduler service through the pub/sub
+			# Never send any frame if `test_mode` is enabled (test_mode=1)
+			if not bool(int(asab.Config["stream:config"]["test_mode"])):
+				t0_publish = time.time()
+				L.warning("[%s] Publishing image into Redis channel: %s" % (get_current_time(), node_channel))
+				dump_request = json.dumps({"active": True, "algorithm": config["algorithm"], "ts": time.time()})
+				pub(self.redis.get_rc(), node_channel, dump_request)
+				t1_publish = (time.time() - t0_publish) * 1000
+				# TODO: Saving latency for scheduler:producer:notification:image
+				L.warning(
+					'[%s] Latency for Publishing FRAME NOTIFICATION into Object Detection Service (%.3f ms)' % (
+						get_current_time(), t1_publish)
 				)
-				L.warning('\n[%s] Proc. Latency of %s for frame-%s (%.3f ms)' % (
-					get_current_time(), "scheduling", str(self.frame_id), t1_sched_lat))
 
-				# Save e2e latency
-				self._exec_e2e_latency_collector(t0_e2e_lat, node_id, self.frame_id)
-
-				# send data into Scheduler service through the pub/sub
-				# Never send any frame if `test_mode` is enabled (test_mode=1)
-				if not bool(int(asab.Config["stream:config"]["test_mode"])):
-					t0_publish = time.time()
-					L.warning("[%s] Publishing image into Redis channel: %s" % (get_current_time(), node_channel))
-					dump_request = json.dumps({"active": True, "algorithm": config["algorithm"], "ts": time.time()})
-					pub(self.redis.get_rc(), node_channel, dump_request)
-					t1_publish = (time.time() - t0_publish) * 1000
-					# TODO: Saving latency for scheduler:producer:notification:image
-					L.warning(
-						'[%s] Latency for Publishing FRAME NOTIFICATION into Object Detection Service (%.3f ms)' % (
-							get_current_time(), t1_publish)
-						)
-
-					if not bool(int(asab.Config["stream:config"]["convert_img"])):
-						# Sending image data through ZMQ (TCP connection)
-						self.ZMQService.send_this_image(senders["zmq"][sel_node_id], self.frame_id, frame)
+				if not bool(int(asab.Config["stream:config"]["convert_img"])):
+					# Sending image data through ZMQ (TCP connection)
+					self.ZMQService.send_this_image(senders["zmq"][sel_node_id], self.frame_id, frame)
+				else:
+					# TODO: In this case, Candidate Selection Algorithm will not work!!!!!
+					# Convert the yolo input images; Here it converts from FullHD into <img_size> (padded size)
+					if not bool(int(asab.Config["stream:config"]["gpu_converter"])):
+						yolo_frame = await self.ResizerService.cpu_convert_to_padded_size(frame)
 					else:
-						# TODO: In this case, Candidate Selection Algorithm will not work!!!!!
-						# Convert the yolo input images; Here it converts from FullHD into <img_size> (padded size)
-						if not bool(int(asab.Config["stream:config"]["gpu_converter"])):
-							yolo_frame = await self.ResizerService.cpu_convert_to_padded_size(frame)
-						else:
-							# NOT IMPLEMENTED YET!!!!
-							# TODO: To add GPU-based downsample function
-							yolo_frame = await self.ResizerService.gpu_convert_to_padded_size(frame)
+						# NOT IMPLEMENTED YET!!!!
+						# TODO: To add GPU-based downsample function
+						yolo_frame = await self.ResizerService.gpu_convert_to_padded_size(frame)
 
-						# CHECKING: how is the latency if we send converted version?
-						# Sending image data through ZMQ (TCP connection)
-						self.ZMQService.send_this_image(senders["zmq"][sel_node_id], self.frame_id, yolo_frame)
-
-		except Exception as e:
-			L.error("[ERROR] extractor/service.py > def extract_image_tcp(): %s" % str(e))
-			return False
-			# TODO: To have further actions, i.e. restart connection (work for both Video file / Streaming
-			# TODO: When reloaded, we need to clean up: RedisDB and any other storage related to this action
+					# CHECKING: how is the latency if we send converted version?
+					# Sending image data through ZMQ (TCP connection)
+					self.ZMQService.send_this_image(senders["zmq"][sel_node_id], self.frame_id, yolo_frame)
 
 	async def _set_cap(self, config):
 		if bool(int(asab.Config["stream:config"]["thread"])):
