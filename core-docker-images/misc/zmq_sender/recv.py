d550e429f0e6d3bc71cba95e0ad3b49a2363b50d
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2020-11-15 01:31:15 +0800

    bugfix: wrong cv2 window title

--- core-docker-images/misc/zmq_sender/recv.py
+++ core-docker-images/misc/zmq_sender/recv.py
@@ -9,10 +9,15 @@ L = logging.getLogger(__name__)
 
 ###
 
-image_hub = imagezmq.ImageHub(open_port='tcp://localhost:5552', REQ_REP=False)
+image_hub = imagezmq.ImageHub(open_port='tcp://localhost:5548', REQ_REP=False)
 
+window_title = "CV Output"
 while True:  # show received images
-    rpi_name, image = image_hub.recv_image()
-    print(">> rpi_name:", rpi_name)
-    # cv2.imshow(rpi_name, image)  # 1 window for each unique RPi name
-    # cv2.waitKey(1)
+    data, image = image_hub.recv_image()
+    print(">> data:", data)
+    cv2.imshow(window_title, image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
