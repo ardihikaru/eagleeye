aa5b6e7d9fdb6e00754a3351c0467889686d65ed
Tim William <timothywilliam.cs06g@g2.nctu.edu.tw>
2020-11-16 05:03:07 +0000

    stable version of iMEC deployment (2.3)

--- orchestration-gpu/deployment/start-ee-deployment.sh
+++ orchestration-gpu/deployment/start-ee-deployment.sh
@@ -1,19 +1,25 @@
 echo "[START] Starting EagleEYE deployment ..."
+kubectl delete deployments --all
+kubectl delete svc --all
+kubectl delete configmap --all
 
 echo "[INFO] Init databases ..."
 kubectl apply -f redis.yaml
-sleep 3
+#kubectl apply -f redis/redis-service.yaml
+sleep 2
 
 kubectl apply -f mongo.yaml
-sleep 3
+#kubectl apply -f mongo/mongo-service.yaml
+sleep 2
 
 echo "[INFO] Web Service ..."
 kubectl apply -f ews.yaml
+#kubectl apply -f ews/ews-service.yaml
 sleep 15
 
 echo "[INFO] Worker-1 ..."
 kubectl apply -f detection.yaml
-sleep 5
+sleep 10
 
 echo "[INFO] Registering Worker-1 ..."
 sh register-node.sh
@@ -21,7 +27,7 @@ sleep 2
 
 echo "[INFO] Worker-2 ..."
 kubectl apply -f detection-2.yaml
-sleep 5
+sleep 10
 
 echo "[INFO] Registering Worker-2 ..."
 sh register-node.sh
@@ -29,18 +35,20 @@ sleep 2
 
 echo "[INFO] Worker-3 ..."
 kubectl apply -f detection-3.yaml
-sleep 5
+sleep 10
 
 echo "[INFO] Scheduler ..."
 kubectl apply -f scheduler.yaml
+#kubectl apply -f scheduler/scheduler-service.yaml
 sleep 3
 
 echo "[INFO] Visualizer ..."
 kubectl apply -f visualizer.yaml
-sleep 2
+sleep 3
 
 echo "[INFO] Visualizer RAWWWWR ..."
 kubectl apply -f visualizer-raw.yaml
-sleep 2
+sleep 3
+
 
 echo "[INFO] All things go!"
