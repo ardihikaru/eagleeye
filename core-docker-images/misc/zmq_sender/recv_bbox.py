891ba7f38c7d46992e5526f0656e8475f4567d71
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2020-11-18 11:37:44 +0800

    add original recv file; add resize parameter

---
+++ core-docker-images/misc/zmq_sender/recv_bbox.py
@@ -0,0 +1,27 @@
+import cv2
+import imagezmq
+import logging
+
+###
+
+L = logging.getLogger(__name__)
+
+
+###
+
+image_hub = imagezmq.ImageHub(open_port='tcp://localhost:5548', REQ_REP=False)
+
+window_title = "CV Output"
+width = 1080
+height = 720
+cv2.namedWindow(window_title, cv2.WINDOW_NORMAL)
+cv2.resizeWindow(window_title, width, height)
+while True:  # show received images
+    data, image = image_hub.recv_image()
+    print(">> data:", data)
+    cv2.imshow(window_title, image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
