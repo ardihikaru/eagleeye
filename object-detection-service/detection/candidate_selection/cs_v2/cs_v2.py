1455fc53bd2f1bb31fa3fc1095bd1f6b1de302ff
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2020-12-02 13:43:17 +0800

    bugfix: resolve multiple PiH BBoxes

--- object-detection-service/detection/candidate_selection/cs_v2/cs_v2.py
+++ object-detection-service/detection/candidate_selection/cs_v2/cs_v2.py
@@ -77,7 +77,6 @@ class CSv2(RegionCluster):
 
         person_xywh = np_xyxy2xywh(person_xyxy)
         flag_xywh = np_xyxy2xywh(flag_xyxy)
-        # print(" -------- person_xywh VS flag_xywh:", person_xywh, flag_xywh)
         if flag_xywh[3] > person_xywh[3]:
             return False
         return True
@@ -88,9 +87,28 @@ class CSv2(RegionCluster):
             return True
         return False
 
+    # this function aims to delete stored pair with higher distant
+    # return True if new pair is having shorter distant
+    def _validate_and_delete_higher_distant_pair(self, ignored_flag_id, current_person_idx, current_dist_idx):
+        is_validated = True
+        current_distant = self.saved_dist[current_dist_idx]
+        for flag_id, pair_data in self.flag_pair_candidates.items():
+            if flag_id != ignored_flag_id and current_person_idx in pair_data["id"]:
+                # locate the index of stored person_id, and get its distant
+                for i in range(len(pair_data["id"])):
+                    # if current distant is smaller, then, delete the stored pair data: in key `id` and `dist`
+                    if pair_data["id"][i] == current_person_idx:
+                        if current_distant < pair_data["dist"][i]:
+                            del pair_data["id"][i]
+                            del pair_data["dist"][i]
+                            break  # exit loop once terget data has been deleted
+                        else:
+                            is_validated = False
+                            break  # exit loop once terget data has been deleted
+        return is_validated
+
     def find_pair_candidates(self):
         for cluster_data in self.mapped_obj:
-            # print("****** cluster_data =", cluster_data)
             if len(cluster_data["Person"]) > 0 and len(cluster_data["Flag"]) > 0:
                 for flag_idx in cluster_data["Flag"]:
                     for person_idx in cluster_data["Person"]:
@@ -98,12 +116,14 @@ class CSv2(RegionCluster):
 
                         if self.__is_flag_valid(flag_idx, person_idx) and self.__is_distance_valid(dist_idx):
                             if person_idx not in self.flag_pair_candidates[flag_idx]["id"]:
-                                self.flag_pair_candidates[flag_idx]["id"].append(person_idx)
-                                # dist_idx = str(flag_idx) + "-" + str(person_idx)
-                                self.flag_pair_candidates[flag_idx]["dist"].append(self.saved_dist[dist_idx])
+                                # also, check if similar PiH candidate has been stored before
+                                if self._validate_and_delete_higher_distant_pair(flag_idx, person_idx, dist_idx):
+                                    self.flag_pair_candidates[flag_idx]["id"].append(person_idx)
+                                    self.flag_pair_candidates[flag_idx]["dist"].append(self.saved_dist[dist_idx])
 
     def pair_selection(self):
         for flag_idx, pair_data in self.flag_pair_candidates.items():
+            # if multiple `person_idx` are found in a single flag, then, choose one with closest distance
             if len(pair_data["id"]) > 0:
                 selected_idx = pair_data["dist"].index(min(pair_data["dist"]))
                 selected_person_idx = pair_data["id"][selected_idx]
