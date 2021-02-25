2be021688c67d3f8e6104170347d4130f3a20020
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2021-02-25 15:42:45 +0800

    - fix data type for `stream` field - add new field: `extras` which is based on an Embedded Document

--- web-service/ews/database/config/config.py
+++ web-service/ews/database/config/config.py
@@ -2,8 +2,12 @@
     Class Model for `Configs` Collections
 """
 
-from mongoengine import Document, StringField, DateTimeField, BooleanField
+from mongoengine import Document, StringField, DateTimeField, BooleanField, EmbeddedDocument, EmbeddedDocumentField
 import datetime
+
+
+class Extras(EmbeddedDocument):
+    selector = StringField(default=None)  # used by ZENOH
 
 
 class ConfigModel(Document):
@@ -11,7 +15,11 @@ class ConfigModel(Document):
     uri = StringField(required=True, unique=True)
     algorithm = StringField(required=True)
     scalable = BooleanField(required=True)
-    stream = BooleanField(required=True)
+    stream = StringField(required=True)
+
+    # Source: https://stackoverflow.com/questions/17872836/embedded-documents-issue-with-mongoengine
+    extras = EmbeddedDocumentField(Extras, default=Extras)
+
     created_at = DateTimeField(default=datetime.datetime.now)
     updated_at = DateTimeField(default=datetime.datetime.now)
 
