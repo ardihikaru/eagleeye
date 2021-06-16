4d9b216cf53f48183d1103e58f3469f33562b1e8
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2021-06-16 17:00:54 +0800

    code refactor and add more options

--- core-docker-images/misc/drone_img_publisher/show_img.py
+++ core-docker-images/misc/drone_img_publisher/show_img.py
@@ -2,6 +2,9 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
 window_title = "output-raw"
 cv2.namedWindow(window_title, cv2.WND_PROP_FULLSCREEN)
 # cv2.resizeWindow("Image", 1920, 1080)  # Enter your size
@@ -11,7 +14,7 @@ while cap.isOpened():
 	ret, frame = cap.read()
 
 	if ret:
-		# print(" >>> frame SHAPE:", frame.shape)
+		print(" ## Img SHAPE:", frame.shape)
 		cv2.imshow(window_title, frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
