7590d0cc2794712ab8e6bd62114320dfa1c8bc0f
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2021-09-13 13:38:19 +0800

    fix auto deployment script

--- docker-prune-containers.sh
+++ docker-prune-containers.sh
@@ -3,6 +3,7 @@
 # Capturing expected total number of worker nodes (input arguments)
 NODES=$1
 DELAY=$2
+NUMDRONE=$3
 
 # Verify input
 ## check Total worker nodes
@@ -11,7 +12,7 @@ then
       echo "\$NODES is empty"
       NODES=2  # default value
 else
-      echo "\$NODES is NOT empty"
+      echo "\$NODES is NOT empty (=${NODES})"
 fi
 ## check delay
 if [ -z "$DELAY" ]
@@ -19,7 +20,15 @@ then
       echo "\$DELAY is empty"
       DELAY=2  # default value
 else
-      echo "\$DELAY is NOT empty"
+      echo "\$DELAY is NOT empty (=${DELAY})"
+fi
+## check Total drones
+if [ -z "$NUMDRONE" ]
+then
+      echo "\$NUMDRONE is empty"
+      NUMDRONE=3  # default value
+else
+      echo "\$NUMDRONE is NOT empty (=${NUMDRONE})"
 fi
 
 echo "Deleting redis-service..."
@@ -31,29 +40,33 @@ docker container rm -f mongo-service
 echo "Deleting ews-service..."
 docker container rm -f ews-service
 
-echo "Deleting all detection-service; Total=$1..."
+echo "Deleting all detection-service; Total=${NODES}..."
 for i in $(seq 1 $NODES);
   do docker container rm -f "detection-service-${i}";
-done
-
-echo "Deleting all pv-service; Total=$1..."
-for i in $(seq 1 $NODES);
-  do docker container rm -f "pv-${i}-svc";
-done
-
-echo "Deleting all sorter-service; Total=$1..."
-for i in $(seq 1 $NODES);
-  do docker container rm -f "sorter-${i}-svc";
 done
 
 echo "Deleting pcs-service..."
 docker container rm -f pcs-svc
 
+echo "Deleting all pv-service; Total=${NUMDRONE}..."
+for i in $(seq 1 $NUMDRONE);
+  do docker container rm -f "pv-${i}-svc";
+done
+
+echo "Deleting all sorter-service; Total=${NUMDRONE}..."
+for i in $(seq 1 $NUMDRONE);
+  do docker container rm -f "sorter-${i}-svc";
+done
+
 echo "Deleting offloader-service..."
 docker container rm -f offloader-service
 
-echo "Deleting visualizer-service..."
-docker container rm -f visualizer-service
+echo "Deleting all visualizer-service; Total=${NUMDRONE}..."
+for i in $(seq 1 $NUMDRONE);
+  do
+    docker container rm -f "viz-rtsp-ee-svc-${i}";
+    docker container rm -f "viz-rtsp-raw-svc-${i}";
+done
 
 echo "Deleting eagleeye network..."
 docker network rm eagleeye
--- setup-local-env.sh
+++ setup-local-env.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
 
+# Sample:
+# bash docker-deploy-network-host.sh 6 2 2 "OK"
+# Deploying 6 detection + 2 drones + with 2 sec delays + To enable/disable XHOST
+
 # Capturing Input
 # any value can be given.
-XHOST=$1
+NODES=$1
+DELAY=$2
+NUMDRONE=$3
+XHOST=$4
+
+# Verify input
+## check Total worker nodes
+if [ -z "$NODES" ]
+then
+      echo "\$NODES is empty"
+      NODES=2  # default value
+else
+      echo "\$NODES is NOT empty (=${NODES})"
+fi
+
+## check delay
+if [ -z "$DELAY" ]
+then
+      echo "\$DELAY is empty"
+      DELAY=2  # default value
+else
+      echo "\$DELAY is NOT empty (=${DELAY})"
+fi
+
+## check number of connected drones
+if [ -z "$NUMDRONE" ]
+then
+      echo "\$$NUMDRONE is empty"
+      NUMDRONE=1  # default value
+else
+      echo "\$$NUMDRONE is NOT empty (=${NUMDRONE})"
+fi
 
 echo "Running shell script file: docker-prune-containers.sh"
-sh ./docker-prune-containers.sh 3 2
+sh ./docker-prune-containers.sh ${NODES} ${DELAY} ${NUMDRONE}
 
 # Verify input
 ## check XHOST setup
